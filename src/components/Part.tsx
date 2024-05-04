c1f767ba482c4ffe89a99f369e83e977b259202a
Emanuel Padilla <emanuehl159@gmail.com>
2024-05-03 20:12:58 -0500

    Add exhaustive type checking in Part component

--- src/components/Part.tsx
+++ src/components/Part.tsx
@@ -1,9 +1,45 @@
 import { CoursePart } from "../types";
 
 export function Part({ coursePart }: { coursePart: CoursePart }) {
-  return (
-    <p>
-      {coursePart.name} {coursePart.exerciseCount}
-    </p>
-  );
+  /**
+   * Helper function for exhaustive type checking
+   */
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`,
+    );
+  };
+
+  switch (coursePart.kind) {
+    case "basic":
+      return (
+        <article>
+          <h3>{coursePart.name}</h3>
+          <p>Exercises: {coursePart.exerciseCount}</p>
+          <p>{coursePart.description}</p>
+        </article>
+      );
+
+    case "group":
+      return (
+        <article>
+          <h3>{coursePart.name}</h3>
+          <p>Exercises: {coursePart.exerciseCount}</p>
+          <p>Group Projects: {coursePart.groupProjectCount}</p>
+        </article>
+      );
+
+    case "background":
+      return (
+        <article>
+          <h3>{coursePart.name}</h3>
+          <p>Exercises: {coursePart.exerciseCount}</p>
+          <p>{coursePart.description}</p>
+          <p>Material: {coursePart.backgroundMaterial}</p>
+        </article>
+      );
+
+    default:
+      return assertNever(coursePart);
+  }
 }
